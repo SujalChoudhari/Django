98b75588ff7fb9d7a17eee54c0c6e962916c9291
Sujal <sjlchoudhari@gmail.com>
2022-10-22 10:11:08 +0530

    Admin and Models
    Started working on Admin and Models

--- Ganga/shop/views.py
+++ Ganga/shop/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(requests):
-    return HttpResponse("Shop Index")
+    return HttpResponse("This is blog home")
+
+def about(requests):
+    return HttpResponse("We are at about")
+
+def contact(requests):
+    return HttpResponse("We are at contact")
+
+def tracker(requests):
+    return HttpResponse("We are at tracker")
+
+def search(requests):
+    return HttpResponse("We are at search")
+
+def product(requests):
+    return HttpResponse("We are at product")
+
+def checkout(requests):
+    return HttpResponse("We are at checkout")
+
